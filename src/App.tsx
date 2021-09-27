efaf7af247acd1f374a576dba4a9745afeccee4c
Sorathan Saetang <sorathan@moveplus.co.th>
2021-09-27 10:13:06 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import TodoList from './components/TodoList';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Nav from './Nav';
 import logo from './todologo.png'
 
